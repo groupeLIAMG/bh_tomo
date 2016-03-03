99ae716b08bbb6044583de9813a546a0cb5a96cf
Bernard Giroux <bernard.giroux@ete.inrs.ca>
2016-03-03 17:29:16 -0500

    added check for properties assignment

--- bh_tomo/Borehole.m
+++ bh_tomo/Borehole.m
@@ -10,10 +10,10 @@ classdef Borehole < handle
         Ymax
         Zmax
         Z_surf
+        Z_water
         scont
         acont
         diam
-        Z_water
         fdata
     end
     
@@ -27,17 +27,90 @@ classdef Borehole < handle
             obj.Ymax = 0;
             obj.Zmax = 0;
             obj.Z_surf = 0;
+            obj.Z_water = NaN;
+            obj.diam = 0;
             obj.scont = [];
             obj.acont = [];
-            obj.diam = 0;
-            obj.Z_water = NaN;
-            obj.fdata = [];
+            obj.fdata = [0 0 0;0 0 0];
         end
         function set.name(obj, n)
             if ischar(n)
                 obj.name = n;
             else
                 error('Borehole name must be a string')
+            end
+        end
+        function set.X(obj,value)
+            if isnumeric(value)
+                obj.X = value;
+            else
+                error('Value must be numeric')
+            end
+        end
+        function set.Y(obj,value)
+            if isnumeric(value)
+                obj.Y = value;
+            else
+                error('Value must be numeric')
+            end
+        end
+        function set.Z(obj,value)
+            if isnumeric(value)
+                obj.Z = value;
+            else
+                error('Value must be numeric')
+            end
+        end
+        function set.Xmax(obj,value)
+            if isnumeric(value)
+                obj.Xmax = value;
+            else
+                error('Value must be numeric')
+            end
+        end
+        function set.Ymax(obj,value)
+            if isnumeric(value)
+                obj.Ymax = value;
+            else
+                error('Value must be numeric')
+            end
+        end
+        function set.Zmax(obj,value)
+            if isnumeric(value)
+                obj.Zmax = value;
+            else
+                error('Value must be numeric')
+            end
+        end
+        function set.Z_surf(obj,value)
+            if isnumeric(value)
+                obj.Z_surf = value;
+            else
+                error('Value must be numeric')
+            end
+        end
+        function set.Z_water(obj,value)
+            if isnumeric(value)
+                obj.Z_water = value;
+            else
+                error('Value must be numeric')
+            end
+        end
+        function set.diam(obj,value)
+            if isnumeric(value)
+                obj.diam = value;
+            else
+                error('Value must be numeric')
+            end
+        end
+        function set.fdata(obj,value)
+            if isnumeric(value)
+                if size(value,2)~=3
+                    error('Borehole data should be nData x 3')
+                end
+                obj.fdata = value;
+            else
+                error('Value must be numeric')
             end
         end
     end

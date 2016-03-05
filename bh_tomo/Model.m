374394c784d355a6d0c20e848cea2b3bcff32529
Bernard Giroux <bernard.giroux@ete.inrs.ca>
2016-03-05 07:48:47 -0500

    class to manade 2D and 3D models

---
+++ bh_tomo/Model.m
@@ -0,0 +1,30 @@
+classdef Model < handle
+    %UNTITLED Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        name
+        mogs         %
+        boreholes    % list of boreholes
+        type
+        grid
+        tt_covar     % covariance model - travel time
+        amp_covar    % covariance model - amplitudes
+        inv_res      % inversion results
+    end
+    
+    methods
+        function obj = Model(n)
+            obj.name = n;
+        end
+        function set.name(obj, n)
+            if ischar(n)
+                obj.name = n;
+            else
+                error('Model name must be a string')
+            end
+        end
+    end
+    
+end
+

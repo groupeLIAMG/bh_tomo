926fae7754fdadc3ecccca4d933c1f24cfd7d113
Bernard Giroux <bernard.giroux@ete.inrs.ca>
2016-03-06 22:27:50 -0500

    minor bug fixes, added modEdited event

--- bh_tomo/@MogUI/MogUI.m
+++ bh_tomo/@MogUI/MogUI.m
@@ -1,8 +1,9 @@
 classdef MogUI < handle
     %MOGUI User interface to manage MOGs
-
+    
     properties
-        mogs
+        mogs       % MOGs data
+        air        % air shots data
     end
     properties (Dependent)
         Position
@@ -16,7 +17,7 @@ classdef MogUI < handle
         bhUI
     end
     events
-        mogAdded, mogDeleted
+        mogAdded, mogDeleted, mogEdited
     end
     
     methods
@@ -27,6 +28,7 @@ classdef MogUI < handle
                 error('Give BoreholeUI object as input')
             end
             obj.mogs = Mog.empty;
+            obj.air = AirShots.empty;
             obj.handles.hp = uipanel(varargin{:},...
                 'Title','MOGs',...
                 'Visible','off',...
@@ -42,7 +44,15 @@ classdef MogUI < handle
                 error('Mogs should be objects of class Mog')
             end
             obj.updateList(1)
-            obj.updateUIfields()
+        end
+        function set.air(obj, a)
+            if isa(a, 'AirShots')
+                obj.air = a;
+            else
+                error('air should be objects of class AirShots')
+            end
+%             obj.updateList(1)
+%             obj.updateUIfields()
         end
         function set.Position(obj, p)
             obj.handles.hp.Position = p;
@@ -98,8 +108,14 @@ classdef MogUI < handle
                 obj.handles.popupTx.String = ' ';
                 obj.handles.popupRx.String = ' ';
             end
-            obj.handles.popupTx.Value = 1;
-            obj.handles.popupRx.Value = 1;
+            no = obj.handles.listMogs.Value;
+            if no>0 && no<=length(obj.mogs)
+                obj.handles.popupTx.Value = obj.mogs(no).Tx;
+                obj.handles.popupRx.Value = obj.mogs(no).Rx;     
+            else
+                obj.handles.popupTx.Value = 1;
+                obj.handles.popupRx.Value = 1;
+            end
         end
     end
     
@@ -107,8 +123,13 @@ classdef MogUI < handle
         addComponents(obj)
         resizeUI(obj,varargin)
         addMog(obj,varargin)
+        importMog(obj,varargin)
         updateCoords(obj,varargin)
-        [x, y, z, c] = projectBorehole(fdata, prof, nom)
+        statsTt(obj,varargin)
+        airBefore(obj,varargin)
+        airAfter(obj,varargin)
+        prune(obj,varargin)
+        [x,y,z,c] = projectBorehole(fdata,prof,nom)
         
         function removeMog(obj,varargin)
             no = obj.handles.listMogs.Value;
@@ -125,27 +146,28 @@ classdef MogUI < handle
         function listMogs(obj,varargin)
             obj.updateUIfields()
         end
-        function airBefore(obj,varargin)
-        end
-        function airAfter(obj,varargin)
-        end
         function popupType(obj,varargin)
             no = obj.handles.listMogs.Value;
             if no>0 && no<=length(obj.mogs)
                 obj.mogs(no).type = obj.handles.popupType.type;
                 obj.updateCoords()
+                obj.notify('mogEdited')
             end
         end
         function popupTx(obj,varargin)
             no = obj.handles.listMogs.Value;
             if no>0 && no<=length(obj.mogs)
                 obj.mogs(no).Tx = obj.handles.popupTx.Value;
+                obj.updateCoords(no);
+                obj.notify('mogEdited')
             end
         end
         function popupRx(obj,varargin)
             no = obj.handles.listMogs.Value;
             if no>0 && no<=length(obj.mogs)
                 obj.mogs(no).Rx = obj.handles.popupRx.Value;
+                obj.updateCoords(no);
+                obj.notify('mogEdited')
             end
         end
         function renameMog(obj,varargin)
@@ -155,69 +177,216 @@ classdef MogUI < handle
                 if ~isempty(name)
                     obj.mogs(no).name = char(name);
                     obj.updateList()
+                    obj.notify('mogEdited')
                 end
             end
-        end
-        function importMog(obj,varargin)
         end
         function spectra(obj,varargin)
         end
         function rawData(obj,varargin)
+            no = obj.handles.listMogs.Value;
+            if no>0 && no<=length(obj.mogs)
+                figure
+                in = obj.mogs(no).in;
+                imagesc(1:obj.mogs(no).no_traces(in),...
+                    obj.mogs(no).data.timestp,...
+                    obj.mogs(no).data.rdata(:,in))
+                clim = caxis;
+                cmax = max(abs(clim));
+                caxis([-cmax cmax])
+                colormap ramac_cmap(16)
+                colorbar('peer',gca);
+                xlabel('Trace no')
+                ylabel('Time [ns]')
+            end
         end
         function zop(obj,varargin)
         end
-        function statsTt(obj,varargin)
-        end
         function statsAmp(obj,varargin)
+            no = obj.handles.listMogs.Value;
+            if no>0 && no<=length(obj.mogs)
+                mog = obj.mogs(no);
+                hyp = sqrt( (mog.data.Tx_x-mog.data.Rx_x).^2 + ...
+                    (mog.data.Tx_y-mog.data.Rx_y).^2 + ...
+                    (mog.data.Tx_z-mog.data.Rx_z).^2 );
+                dz = mog.data.Rx_z-mog.data.Tx_z;
+                theta = 180/pi*asin(dz./hyp);
+                
+                h_stat = figure;
+                set(h_stat, 'Position',[256 71 512 620]);
+                
+                ind = mog.amp_tmax ~= -1 & mog.tauApp ~= -1 & mog.amp_done;
+                ind = ind & mog.in;
+                
+                subplot(321)
+                plot(hyp(ind), mog.tauApp(ind),'o')
+                xlabel('Straight ray length [m]')
+                ylabel('\tau_a')
+                title('amplitude - amp. ratio')
+                
+                subplot(322)
+                plot(theta(ind), mog.tauApp(ind)./hyp(ind),'o')
+                xlabel('Angle w/r to horizontal [deg]')
+                ylabel('\alpha_a')
+                title('amplitude - amp. ratio')
+                
+                ind = mog.amp_tmax ~= -1 & mog.tauFce ~= -1 & mog.amp_done;
+                ind = ind & mog.in;
+                
+                subplot(323)
+                plot(hyp(ind), mog.tauFce(ind),'o')
+                xlabel('Straight ray length [m]')
+                ylabel('\tau_a')
+                title('amplitude - centroid freq.')
+                
+                subplot(324)
+                plot(theta(ind), mog.tauFce(ind)./hyp(ind),'o')
+                xlabel('Angle w/r to horizontal [deg]')
+                ylabel('\alpha_a')
+                title('amplitude - centroid freq.')
+                
+                ind = mog.amp_tmax ~= -1 & mog.tauHyb ~= -1 & mog.amp_done;
+                ind = ind & mog.in;
+                
+                subplot(325)
+                plot(hyp(ind), mog.tauHyb(ind),'o')
+                xlabel('Straight ray length [m]')
+                ylabel('\tau_a')
+                title('amplitude - hybrid')
+                
+                subplot(326)
+                plot(theta(ind), mog.tauHyb(ind)./hyp(ind),'o')
+                xlabel('Angle w/r to horizontal [deg]')
+                ylabel('\alpha_a')
+                title('amplitude - hybrid')
+            end
         end
         function rays(obj,varargin)
+            no = obj.handles.listMogs.Value;
+            if no>0 && no<=length(obj.mogs)
+                mog = obj.mogs(no);
+                boreholes = obj.bhUI.boreholes;
+                ind1 = mog.tt==-1 & mog.in;
+                ind2 = mog.tt~=-1 & mog.in;
+                
+                [~, a]=lsplane([boreholes(mog.Tx).fdata; boreholes(mog.Rx).fdata]);
+                el = (pi-a(3))*180/pi - 15;
+                az = atan( cos(a(2))/cos(a(1)) )*180/pi - 10;
+                
+                figure
+                plot3([mog.data.Tx_x(ind1); mog.data.Rx_x(ind1)], ...
+                    [mog.data.Tx_y(ind1); mog.data.Rx_y(ind1)], ...
+                    [mog.data.Tx_z(ind1); mog.data.Rx_z(ind1)],'r')
+                hold on
+                plot3([mog.data.Tx_x(ind2); mog.data.Rx_x(ind2)], ...
+                    [mog.data.Tx_y(ind2); mog.data.Rx_y(ind2)], ...
+                    [mog.data.Tx_z(ind2); mog.data.Rx_z(ind2)],'g')
+                hold off
+                set(gca, 'DataAspectRatio',[1 1 1],'Units','normalized')
+                title([num2str(100*sum(ind2)/length(ind2)), '%'])
+                axis tight
+                xlabel('Tx-Rx Distance [m]','FontSize',8)
+                ylabel('Elevation [m]','FontSize',8)
+                view(az,el)
+            end
         end
         function exportTt(obj,varargin)
+            no = obj.handles.listMogs.Value;
+            if no>0 && no<=length(obj.mogs)
+                [filename, pathname] = uiputfile({'*.dat';'*.*'}, 'Export traveltimes');
+                if isequal(filename,0) || isequal(pathname,0)
+                    return;
+                end
+                mog = obj.mogs(no);
+                done = mog.tt_done & mog.in;
+                if sum(done)==0
+                    errordlg('Data not processed!!!')
+                    return
+                end
+                ind = mog.tt~=-1 & mog.in;
+                [tt,~] = mog.getCorrectedTravelTimes(obj.air);
+                obj.handles.editDtCorr.String = num2str( mog.fac_dt );
+                et = mog.et(ind);
+                tt = tt(ind);
+                
+                data = [mog.data.Tx_x(ind)' mog.data.Tx_y(ind)' mog.data.Tx_z(ind)' ...
+                    mog.data.Rx_x(ind)' mog.data.Rx_y(ind)' mog.data.Rx_z(ind)' ...
+                    tt' et' mog.no_traces(ind)']; %#ok<NASGU>
+                
+                eval(['save ',pathname,filename,' data -ascii'])
+            end
         end
         function exportTau(obj,varargin)
-        end
-        function prune(obj,varargin)
+            no = obj.handles.listMogs.Value;
+            if no>0 && no<=length(obj.mogs)
+                mog = obj.mogs(no);
+                tt = mog.tauApp;
+                et = mog.tauApp_et;
+                ind = tt~=-1 & mog.in;
+                tt = tt(ind);
+                et = et(ind);
+                if isempty(tt)
+                    errordlg('Data not processed!!!')
+                    return
+                end
+                [filename, pathname] = uiputfile({'*.dat';'*.*'}, 'Export reduced amplitudes');
+                if isequal(filename,0) || isequal(pathname,0)
+                    return;
+                end
+                data = [mog.data.Tx_x(ind)' mog.data.Tx_y(ind)' mog.data.Tx_z(ind)' ...
+                    mog.data.Rx_x(ind)' mog.data.Rx_y(ind)' mog.data.Rx_z(ind)' ...
+                    tt' et' mog.no_traces(ind)']; %#ok<NASGU>
+                
+                eval(['save ',pathname,filename,' data -ascii'])
+            end
         end
         function editDate(obj,varargin)
             no = obj.handles.listMogs.Value;
             if no>0 && no<=length(obj.mogs)
                 obj.mogs(no).data = obj.handles.editDate.String;
+                obj.notify('mogEdited')
             end
         end
         function editFreq(obj,varargin)
             no = obj.handles.listMogs.Value;
             if no>0 && no<=length(obj.mogs)
                 obj.mogs(no).data.rnomfreq = str2double(obj.handles.editFreq.String);
+                obj.notify('mogEdited')
             end
         end
         function editFeedRx(obj,varargin)
             no = obj.handles.listMogs.Value;
             if no>0 && no<=length(obj.mogs)
                 obj.mogs(no).data.RxOffset = str2double(obj.handles.editFeedRx.String);
+                obj.notify('mogEdited')
             end
         end
         function editFeedTx(obj,varargin)
             no = obj.handles.listMogs.Value;
             if no>0 && no<=length(obj.mogs)
                 obj.mogs(no).data.TxOffset = str2double(obj.handles.editFeedTx.String);
+                obj.notify('mogEdited')
             end
         end
         function checkDtCorr(obj,varargin)
             no = obj.handles.listMogs.Value;
             if no>0 && no<=length(obj.mogs)
                 obj.mogs(no).user_fac_dt = obj.handles.checkDtCorr.Value;
+                obj.notify('mogEdited')
             end
         end
         function editDtCorr(obj,varargin)
             no = obj.handles.listMogs.Value;
             if no>0 && no<=length(obj.mogs)
                 obj.mogs(no).fac_dt = str2double(obj.handles.editDtCorr.String);
+                obj.notify('mogEdited')
             end
         end
         function editMultFac(obj,varargin)
             no = obj.handles.listMogs.Value;
             if no>0 && no<=length(obj.mogs)
                 obj.mogs(no).f_et = str2double(obj.handles.editMultFac.String);
+                obj.notify('mogEdited')
             end
         end
         function updateList(obj,varargin)
@@ -253,8 +422,12 @@ classdef MogUI < handle
                 obj.handles.popupType.Value = obj.mogs(no).type;
                 obj.handles.popupRx.Value = obj.mogs(no).Rx;
                 obj.handles.popupTx.Value = obj.mogs(no).Tx;
-            
-            % TODO add air shot 
+                if ~isempty(obj.mogs(no).av)
+                    obj.handles.textAirBefore.String = obj.air(obj.mogs(no).av).name;
+                end
+                if ~isempty(obj.mogs(no).ap)
+                    obj.handles.textAirAfter.String = obj.air(obj.mogs(no).ap).name;
+                end
             end
         end
     end

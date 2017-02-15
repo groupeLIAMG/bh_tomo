5fc2879a0f7a6e06244c8cc2443ab58fd526e45e
Bernard Giroux <bernard.giroux@ete.inrs.ca>
2017-02-15 09:51:42 -0500

    added method to copy handle object

--- bh_tomo/MogData.m
+++ bh_tomo/MogData.m
@@ -53,6 +53,19 @@ classdef MogData < handle
             obj.date = '';
             obj.tdata = [];
         end
+        
+        % Make a copy of a handle object.
+        function new = copy(obj)
+            % Instantiate new object of the same class.
+            new = feval(class(obj));
+            
+            % Copy all non-hidden properties.
+            p = properties(obj);
+            for i = 1:length(p)
+                new.(p{i}) = obj.(p{i});
+            end
+        end
+
         function s = getSubset(obj,ind)
             s = MogData;
             s.ntrace = sum(ind);
